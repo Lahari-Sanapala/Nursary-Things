a04b8b70a065394fabe5d3fbc88097fda1b2a05e
Lahari-Sanapala <laharisanapala2005@gmail.com>
2024-12-18 22:41:03 +0530

    Adjusted cursor and follower behavior

--- script.js
+++ script.js
@@ -34,4 +34,49 @@ document.querySelectorAll('.nav-link').forEach(link => {
       behavior: 'smooth'
     });
   });
-  
+
+
+
+
+
+// Define the cursor and follower elements
+var cursor = $(".cursor"),
+    follower = $(".cursor-follower");
+
+// Variables to track positions
+var posX = 0,
+    posY = 0;
+
+var mouseX = 0,
+    mouseY = 0;
+
+// Animation loop to smoothly move the follower
+TweenMax.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function() {
+        posX += (mouseX - posX) / 4; // Faster response for the follower
+        posY += (mouseY - posY) / 4;
+
+        // Update follower position
+        TweenMax.set(follower, {
+            css: {
+                left: posX - 5, // Reduced offset for closer overlap
+                top: posY - 5
+            }
+        });
+
+        // Update cursor position
+        TweenMax.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY
+            }
+        });
+    }
+});
+
+// Update mouse positions on movement
+$(document).on("mousemove", function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+});
