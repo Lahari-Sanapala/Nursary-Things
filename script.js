465bd9e4def5dbaaa59d6ff11f154bb84c28ad41
Lahari-Sanapala <laharisanapala2005@gmail.com>
2024-12-19 11:30:36 +0530

    final cursor

--- script.js
+++ script.js
@@ -36,44 +36,46 @@ document.querySelectorAll('.nav-link').forEach(link => {
   });
 
 
-// Define the cursor and follower elements
-var cursor = $(".cursor"),
-    follower = $(".cursor-follower");
 
-// Variables to track positions
+
+
+
+  var cursor = $(".cursor"),
+  follower = $(".cursor-follower");
+
 var posX = 0,
-    posY = 0;
+  posY = 0;
 
 var mouseX = 0,
-    mouseY = 0;
+  mouseY = 0;
 
-// Animation loop to smoothly move the follower
 TweenMax.to({}, 0.016, {
-    repeat: -1,
-    onRepeat: function() {
-        posX += (mouseX - posX) / 4; // Faster response for the follower
-        posY += (mouseY - posY) / 4;
-
-        // Update follower position
-        TweenMax.set(follower, {
-            css: {
-                left: posX - 5, // Reduced offset for closer overlap
-                top: posY - 5
-            }
-        });
-
-        // Update cursor position
-        TweenMax.set(cursor, {
-            css: {
-                left: mouseX,
-                top: mouseY
-            }
-        });
+repeat:-1,
+onRepeat:function(){
+  posX += (mouseX - posX) / 9;
+  posY += (mouseY - posY) / 9;
+  
+  TweenMax.set(follower, {
+    css: {
+      left: posX - 12,
+      top: posY - 12
     }
+  });
+  TweenMax.set(cursor, {
+    css: {
+      left: mouseX,
+      top: mouseY
+    }
+  });
+}	
 });
 
-// Update mouse positions on movement
-$(document).on("mousemove", function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
+$(document).on("mousemove",function(e){
+mouseX = e.pageX;
+mouseY = e.pageY;
 });
+
+
+
+
+
